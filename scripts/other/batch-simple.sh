718103526a1552e663a1dcbf7a2878669cfdda07
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2020-07-07 09:31:33 +0200

    preparing scripts to run on ARIS

---
+++ scripts/other/batch-simple.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+####################################
+#     ARIS slurm script template   #
+#                                  #
+# Submit script: sbatch filename   #
+#                                  #
+####################################
+
+#SBATCH --partition=gpu    # Partition
+#SBATCH --account=pa200702    # Replace with your system project
+
+if [ x$SLURM_CPUS_PER_TASK == x ]; then
+  export OMP_NUM_THREADS=1
+else
+  export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+fi
+
+source $HOME/.bashrc
+
+$@
+
+
