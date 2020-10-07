b89562698fa58969dfa10487faac5f02ff0f088c
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2020-10-07 12:30:19 +0300

    debugging beam_fb

--- blond/gpu/gpu_physics_wrap.py
+++ blond/gpu/gpu_physics_wrap.py
@@ -269,9 +269,11 @@ def gpu_beam_phase(bin_centers, profile, alpha, omega_rf, phi_rf, ind, bin_size)
                   array1, array2, np.int32(n_bins),
                   block=block_size)  # , grid=grid_size)
 
-    # beam_phase_sum(array1, array2, dev_scoeff, dev_coeff,
-    #                np.int32(n_bins), block=block_size,
-    #                grid=(1, 1, 1), time_kernel=True)
-    to_ret = dev_scoeff[0].get()
-    print("to_ret: %f" % to_ret)
+    beam_phase_sum(array1, array2, dev_scoeff, dev_coeff,
+                   np.int32(n_bins), block=block_size,
+                   grid=(1, 1, 1), time_kernel=True)
+    # to_ret = dev_scoeff[0].get()
+    # to_ret = array1[0].get()
+    to_ret = 0
+    print(f"Array1:{array1[0].get()} , array2: {array2[0].get()}")
     return to_ret
